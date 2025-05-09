f1efb5966026be75e731d43c598e76fa48e7423b
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-09 08:37:36 +0700

    menambahkan void traverse dan display data didalam list

--- file1.cpp
+++ file1.cpp
@@ -87,6 +87,28 @@ public:
             START = START->next;
         else
             previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
     }
 };
 
